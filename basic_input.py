ea025b490a9308030a8dbafda54bdc8cf5685356
Amit kumar <amithanukittomnk.rajpal@gmail.com>
2025-12-23 20:09:57 +0530

    Added if else condition for age eligibility

--- basic_input.py
+++ basic_input.py
@@ -13,3 +13,11 @@ num2 = int(input("Enter second number: "))
 
 total = num1 + num2
 print("Sum of two numbers is:", total)
+# Check eligibility based on age
+
+age = int(input("Enter your age again for eligibility check: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
